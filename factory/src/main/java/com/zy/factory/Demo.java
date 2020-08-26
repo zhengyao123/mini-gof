a407ff10253ebb40def5e873298e0e00ec909fb3
zhengyao <1132201989@qq.com>
2020-08-26 16:27:11 +0800

    commit

---
+++ factory/src/main/java/com/zy/factory/Demo.java
@@ -0,0 +1,12 @@
+package com.zy.factory;
+
+/**
+ * date:  2020-08-26 16:25
+ *
+ * @author zhengyao
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
